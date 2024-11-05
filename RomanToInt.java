504e1ad0623b074a3e1d15ab9ba6e48012f24b2b
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2024-11-05 17:28:22 +0530

    LEETCODE -> 16 Roman To Integer

---
+++ RomanToInt.java
@@ -0,0 +1,32 @@
+public class RomanToInt {
+        public int romanToInt(String s) {
+            int[] values = new int[26];
+            values['I' - 'A'] = 1;
+            values['V' - 'A'] = 5;
+            values['X' - 'A'] = 10;
+            values['L' - 'A'] = 50;
+            values['C' - 'A'] = 100;
+            values['D' - 'A'] = 500;
+            values['M' - 'A'] = 1000;
+
+            int total = 0;
+            int length = s.length();
+
+            for (int i = 0; i < length; i++) {
+                int currentValue = values[s.charAt(i) - 'A'];
+                if (i + 1 < length && currentValue < values[s.charAt(i + 1) - 'A']) {
+                    total -= currentValue;
+                } else {
+                    total += currentValue;
+                }
+            }
+            return total;
+        }
+
+        public static void main(String[] args) {
+            RomanToInt converter = new RomanToInt();
+            System.out.println(converter.romanToInt("III"));     // Output: 3
+            System.out.println(converter.romanToInt("LVIII"));   // Output: 58
+            System.out.println(converter.romanToInt("MCMXCIV")); // Output: 1994
+        }
+}
