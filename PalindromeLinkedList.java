f7b66084e0604a991c686c32be974801102c20a9
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2024-11-12 23:32:54 +0530

    LEETCODE -> 234 Palindrome Linked List

---
+++ PalindromeLinkedList.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class PalindromeLinkedList {
+    public ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode midNode = findMid(head);
+
+        ListNode prev = null;
+        ListNode curr = midNode;
+        ListNode next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode right = prev;
+        ListNode left = head;
+        while (right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ListNode node4 = new ListNode(1);
+        ListNode node3 = new ListNode(2, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode head = new ListNode(1, node2);
+
+        PalindromeLinkedList palindrome = new PalindromeLinkedList();
+        boolean result = palindrome.isPalindrome(head);
+
+        System.out.println("Is the linked list a palindrome? " + result);  // Output: true
+    }
+}
