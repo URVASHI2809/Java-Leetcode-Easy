9ac8c4a4f718540738b04104c5962beacfa31c0a
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-08-13 16:29:47 +0530

    LEETCODE -> 326. Power of Three

---
+++ IsPowerOfThree.java
@@ -0,0 +1,7 @@
+class IsPowerOfThree {
+    public boolean isPowerOfThree(int n) {
+        int e = (int)(Math.log(Integer.MAX_VALUE) / Math.log(3));
+        int N = (int)Math.pow(3, e);
+        return n > 0 && N % n == 0;
+    }
+}
